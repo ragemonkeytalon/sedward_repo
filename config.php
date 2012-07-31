7e66e5c66167cb45c1f5b8a1e034d6cdf70735ec
ragemonkeytalon <ragedookie@yahoo.com>
2012-07-31 12:15:22 -0600

    Version 00.00.002 : Added config.php file and include statement within index.php. Tracks version information and recent changes. Done in common form.

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+// config.php
+
+
+echo "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>";
+echo "<html xmlns='http://www.w3.org/1999/xhtml'>
+echo "<head>";
+echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />";
+echo "<meta name='' />"; 
+echo "</head>";
+echo "<title>";
+
+
+echo "Test Repo Page -- Version 00.00.002";
+
+echo "</title>";
+
+
+/*
+
+Added in this revision:
+
+
+Added config.php file that tracks version information and places that information in the title bar
+
+
+
+
+?*
--- index.php
+++ index.php
@@ -14,13 +14,8 @@ include "logged_in.php";
 }
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="" /> 
-</head>
-<title>Welcome to My Simple Test Site </title>
+include "config.php";
+
 
 <style type="text/css">
 <!--
